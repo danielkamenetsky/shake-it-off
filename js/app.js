786558567fc7b95fe1123267d7f289589f3bfb79
Daniel Kamenetsky <20dk19@queensu.ca>
2024-08-27 13:01:57 -0400

    Created createQuote, appendQuote, and redner functions to modularize quote handling logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 /*---- Constants ----*/
-import { getRandomQuote, clown } from "../data/quotes.js"
+import { getRandomQuote} from "../data/quotes.js"
 import * as taylorAudio from './audio.js'
 
-console.log(clown)
-console.log(taylorAudio)
 
 /** variables */
 
@@ -14,18 +12,47 @@ const quotes = []
 const quoteBtn = document.querySelector('#quote-button') 
 const cardContainer = document.querySelector('#card-container')
 
-console.log(quoteBtn, cardContainer)
 
 
 
 /*------Event LIsteners---*/
 
-quoteBtn.addEventListener('click', () => {
-  const newQuote = getRandomQuote()
-  quotes.push(newQuote)
-  console.log(quotes)
-})
+quoteBtn.addEventListener('click', createQuote)
 
 
 
 /*-----Functions ----*/
+function createQuote() {
+  const newQuote = getRandomQuote()
+  quotes.push(newQuote)
+  console.log(quotes)
+  render()
+}
+
+
+function render() {
+  cardContainer.innerHTML = ''
+  quotes.forEach(quote => {
+    appendQuote(quote)
+  })
+}
+
+function appendQuote(quote) { 
+  // Create a new div element to hold the quote
+  let quoteCard = document.createElement('div')
+  
+  // Add a class name "card" to the div for styling
+  quoteCard.className = `card`
+  
+  // Set the content inside the div using the quote's text
+  // If the quote is from Taylor Swift, show "T-Swift"
+  // Otherwise, show "Hater Tot"
+  quoteCard.innerHTML = `
+    <div> 
+    <p> ${quote.text} </p>
+    <p> ${quote.isTaylor ? '--- T-Swift' :'--Hater Tot'}</p>
+    </div>`
+  
+  // Add the div (quote card) to the main container on the page
+  cardContainer.appendChild(quoteCard)
+}
